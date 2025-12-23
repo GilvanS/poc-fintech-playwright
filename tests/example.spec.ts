7590e73463e85500e4a87e7363aa0c53254782d2
GilvanJSSousa <gillvanjs@gmail.com>
2025-12-23 10:45:03 -0300

    rebase

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,9 +3,6 @@ import { test, expect } from '@playwright/test';
 test('Abrir site', async ({ page }) => {
   await page.goto('/');
 
-  const titulo = {
-    title: 'Olá!'
-  }
   const loggedUsed = page.locator('.text-2xl')
   await expect(loggedUsed).toHaveText('Olá!');
   const msgNaoECliente = page.locator('.py-3')
