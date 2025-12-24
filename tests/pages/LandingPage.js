4caadd973fe0963c71b3fcbf985663c1e38dbb05
GilvanJSSousa <gillvanjs@gmail.com>
2025-12-24 12:56:48 -0300

    test: POM

--- tests/pages/LandingPage.js
+++ tests/pages/LandingPage.js
@@ -8,7 +8,7 @@ export class LandingPage {
 
     async visit(message = 'Olá!') {
         await this.page.goto('/');
-        const loggedUsed = this.page.locator('.text-2xl');
+        const loggedUsed = this.page.locator('#prelogin-greeting');
         await expect(loggedUsed).toHaveText(message);
     }
 
@@ -19,16 +19,16 @@ export class LandingPage {
 
     async validMsg() {
         const message = 'Não é cliente? Abra uma conta'
-        const msgNaoECliente = this.page.locator('.py-3')
+        const msgNaoECliente = this.page.locator('#btn-signup')
         await expect(msgNaoECliente).toHaveText(message)
     }
 
     async submitLoginForm(cpf, password) {
         if (cpf) {
-            await this.page.getByPlaceholder('000.000.000-00').fill(cpf);
+            await this.page.locator('#login-cpf').fill(cpf);
         }
         if (password) {
-            await this.page.locator('#password-login').fill(password);
+            await this.page.locator('#login-password').fill(password);
         }
         await this.page.getByRole('button', { name: 'Entrar' }).click();
     }
